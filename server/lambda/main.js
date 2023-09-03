3081556fe3c14defab94b70965abc0e73a82b88e
nika <jamaspishvilinika@icloud.com>
2023-09-03 17:42:42 +0400

    commit

--- server/lambda/main.js
+++ server/lambda/main.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
+const Router=express.Router();
 const cors=require('cors');
 const serverLess=require('serverless-http');
 
 app.use(cors({origin:'http://127.0.0.1:5500'}));
 app.use(express.json());
 
-app.post('/.netlify/functions/main',(req,res)=>{
+
+Router.post('/.netlify/functions/main',(req,res)=>{
     res.json('hello world again');
     console.log(req.body.message);
 })
 
+app.use('/',Router);
 exports.handler =serverLess(app);
   
